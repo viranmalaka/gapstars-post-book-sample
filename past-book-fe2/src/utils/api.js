b1e69fb39408ee734ae7a5e5f5290c46d8d5bd09
viranmalaka <malaka.14@cse.mrt.ac.lk>
2021-06-30 12:02:28 +0530

    update the routes

--- past-book-fe2/src/utils/api.js
+++ past-book-fe2/src/utils/api.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import to from './await-to';
-const API_ROOT = 'http://localhost:3000/api/v1/'; // root of the api url
+import axios from "axios";
+import to from "./await-to";
+const API_ROOT = "/api/v1/"; // root of the api url
 
 const responseBody = (res) => res.data; // an arrow function to takes only the data value from the response object
 const errBody = (res) => res.response.data;
@@ -14,14 +14,29 @@ const axiosConfig = (headers) => ({
 const createAPI = (apiRoot) => {
   return {
     get: (url, headers = {}) =>
-      to(axios.get(`${apiRoot}${url}`, axiosConfig(headers)).then(responseBody).catch(errBody)),
+      to(
+        axios
+          .get(`${apiRoot}${url}`, axiosConfig(headers))
+          .then(responseBody)
+          .catch(errBody)
+      ),
     post: (url, body, headers = {}) =>
-      to(axios.post(`${apiRoot}${url}`, body, axiosConfig(headers)).then(responseBody).catch(errBody)),
+      to(
+        axios
+          .post(`${apiRoot}${url}`, body, axiosConfig(headers))
+          .then(responseBody)
+          .catch(errBody)
+      ),
     patch: (url, body, headers = {}) =>
-      to(axios.patch(`${apiRoot}${url}`, body, axiosConfig(headers)).then(responseBody).catch(errBody)),
+      to(
+        axios
+          .patch(`${apiRoot}${url}`, body, axiosConfig(headers))
+          .then(responseBody)
+          .catch(errBody)
+      ),
   };
 };
 
 const API = createAPI(API_ROOT);
-export const OutsideAPI = createAPI('');
+export const OutsideAPI = createAPI("");
 export default API;
