09c0903077abaddecfb951380ecd6fff576083fc
viranmalaka <malaka.14@cse.mrt.ac.lk>
2021-06-30 12:07:14 +0530

    update the routes

--- past-book-fe2/src/utils/constants.js
+++ past-book-fe2/src/utils/constants.js
@@ -1,4 +1,4 @@
 // keep track of the static user id to save selected images as there's no authentication implemented.
-export const STATIC_AUTHOR_ID = process.env.REACT_APP_STATIC_AUTHOR_ID;
+export const STATIC_AUTHOR_ID = "1234567";
 
 export const USER_IMAGES_URL = process.env.REACT_APP_USER_IMAGES_URL;
